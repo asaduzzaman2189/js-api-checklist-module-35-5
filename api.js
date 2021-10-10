6763defba46988c50a0c341161fca835b169525f
asaduzzaman2189 <asaduzzaman2189@gmail.com>
2021-10-10 09:36:43 +0600

    35_5-3 Add Spinner And Toggle Content While Loading Data

--- api.js
+++ api.js
@@ -9,6 +9,27 @@ const displaySingleUser = (user) => {
   console.log(user);
 };
 
+//meal db
+//toggle spinner function
+const toggleSpinner = (displayStyle) => {
+  document.getElementById("spinner").style.display = displayStyle;
+};
+const toggleSearchResult = (displayStyle) => {
+  document.getElementById("meals").style.display = displayStyle;
+};
+const searchMeal = () => {
+  const searchText = document.getElementById("search-field").value;
+
+  // display spinner-toggle spinner function call
+  toggleSpinner("block");
+
+  //আগের সার্চ রেজাল্ট দেখাব না তাই
+  toggleSearchResult("none");
+
+  loadMeals(searchText);
+  document.getElementById("search-field").value = "";
+};
+
 const loadMeals = (searchText) => {
   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
   fetch(url)
@@ -18,18 +39,29 @@ const loadMeals = (searchText) => {
 
 const displayMeals = (meals) => {
   const container = document.getElementById("meals");
-  meals.forEach((meal) => {
+  container.textContent = "";
+  if (!meals) {
+    // console.log("Oops!"); div তৈরি করে এটা দেখাবে
+  }
+  meals?.forEach((meal) => {
     console.log(meal);
     const div = document.createElement("div");
     div.innerHTML = `
     <h1>${meal.strMeal}</h1>
+    <p>${meal.strIngredient18 ? meal.strIngredient18 : "Not available"}</p>
     <button onclick="loadMealDetail('${meal.strMeal}')">Click Me</button>
     `;
     container.appendChild(div);
   });
+
+  //toggle spinner off
+  toggleSpinner("none");
+
+  //toggleSearchResult দেখানোর জন্য-
+  toggleSearchResult("block");
 };
 
-loadMeals("fish");
+loadMeals();
 
 const loadMealDetail = (mealName) => {
   console.log(mealName);
